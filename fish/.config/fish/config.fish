3c585ac4d10c785c4706cae9d70b1d3b19c7d7b5
Caleb St-Denis <caleb.stdenis@gmail.com>
2021-04-06 11:39:00 -0400

    Go variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,4 +4,9 @@ bind \t forward-word
 bind \e\t complete
 
 set GRADLE_USER_HOME "~/.gradle"
+set -x GOPATH $HOME/golang
+set -x GOROOT /usr/local/opt/go/libexec
+set -x GO111MODULE on
+set PATH $GOPATH/bin $GOROOT/bin $PATH
+
 add_secrets
