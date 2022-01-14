0bf83a1bbc5f81fbdcd91159c961325391542733
Caleb St-Denis <caleb.stdenis@gmail.com>
2022-01-14 09:25:46 -0500

    Add touchpad scroll support to Git

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,13 @@
-set -U fish_greeting
-
-bind \t forward-word
-bind \e\t complete
-
 set GRADLE_USER_HOME "~/.gradle"
+
 set -x GOPATH $HOME/golang
 set -x GOROOT /usr/local/opt/go/libexec
 set -x GO111MODULE on
 set PATH $GOPATH/bin $GOROOT/bin $PATH
 
+set EDITOR code -w
+
 add_secrets
+
+# Set default directory
+cd ~/src/ea/r
